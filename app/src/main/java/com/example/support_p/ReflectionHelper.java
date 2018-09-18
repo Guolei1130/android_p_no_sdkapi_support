8e2f3718cc5471181dffbd72f2cd2259b6e12fca
guolei2 <vipkid@vipkiddeMacBook-Pro.local>
2018-09-18 18:40:59 +0800

    remove no use

--- app/src/main/java/com/example/support_p/ReflectionHelper.java
+++ app/src/main/java/com/example/support_p/ReflectionHelper.java
@@ -1,15 +1,4 @@
 package com.example.support_p;
-//       _       _    _     _
-//__   _(_)_ __ | | _(_) __| |
-//\ \ / / | '_ \| |/ / |/ _` |
-// \ V /| | |_) |   <| | (_| |
-//  \_/ |_| .__/|_|\_\_|\__,_|
-//        |_|
-
-/* guolei2@vipkid.com.cn
- *
- *2018/9/17
- */
 
 import android.util.Log;
 import java.lang.reflect.Field;
